d2bffe11da8bc8ea86ad2092d23729393cb3d59e
Bozhenko Roman <35889412+BozhenkoR@users.noreply.github.com>
2023-02-01 19:50:04 +0200

    5 commit

--- app/src/main/java/com/example/mycleanmvvm/data/api/model/CompanyNetworkModel.kt
+++ app/src/main/java/com/example/mycleanmvvm/data/api/model/CompanyNetworkModel.kt
@@ -3,7 +3,7 @@ package com.example.mycleanmvvm.data.api.model
 import com.google.gson.annotations.Expose
 import com.google.gson.annotations.SerializedName
 
-class ComapanyNetworkModel(
+class CompanyNetworkModel(
     @SerializedName("name")
     val name: String,
     @SerializedName("catchPhrase")
